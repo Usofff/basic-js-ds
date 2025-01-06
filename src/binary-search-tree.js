030a250d3ed6b369e171c08671b395adc5f0ebf8
Ilya Usov <iusov41@gmail.com>
2025-01-06 22:33:44 +0300

    feat: solve tree task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,41 +7,135 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = { data, left: null, right: null };
+
+    if (!this.rootNode) {
+      this.rootNode = newNode;
+      return this;
+    }
+
+    let current = this.rootNode;
+    while (true) {
+      if (data < current.data) {
+        if (!current.left) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else if (data > current.data) {
+        if (!current.right) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      } else {
+        return this; // Duplicate value, do nothing
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    let current = this.rootNode;
+    while (current) {
+      if (data < current.data) {
+        current = current.left;
+      } else if (data > current.data) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let current = this.rootNode;
+    while (current) {
+      if (data < current.data) {
+        current = current.left;
+      } else if (data > current.data) {
+        current = current.right;
+      } else {
+        return current;
+      }
+    }
+    return null;
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootNode) {
+      return null;
+    }
+    let current = this.rootNode;
+    while (current.left) {
+      current = current.left;
+    }
+    return current.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootNode) {
+      return null;
+    }
+    let current = this.rootNode;
+    while (current.right) {
+      current = current.right;
+    }
+    return current.data;
   }
+
+    remove(data) {
+        this.rootNode = this._removeNode(this.rootNode, data);
+        return this;
+    }
+
+    _removeNode(node, data) {
+        if (!node) {
+            return null;
+        }
+
+        if (data < node.data) {
+            node.left = this._removeNode(node.left, data);
+            return node;
+        } else if (data > node.data) {
+            node.right = this._removeNode(node.right, data);
+            return node;
+        } else {
+            // Node to be deleted found
+
+            // Case 1: Node with no children
+            if (!node.left && !node.right) {
+                return null;
+            }
+
+            // Case 2: Node with one child
+            if (!node.left) {
+                return node.right;
+            }
+            if (!node.right) {
+                return node.left;
+            }
+
+            // Case 3: Node with two children
+            // Find the inorder successor (smallest node in the right subtree)
+            let temp = node.right;
+            while (temp.left) {
+                temp = temp.left;
+            }
+            node.data = temp.data; // Copy the inorder successor's data to the node
+            node.right = this._removeNode(node.right, temp.data); // Delete the inorder successor
+            return node;
+        }
+    }
 }
 
 module.exports = {
